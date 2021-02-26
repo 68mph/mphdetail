8725ec891131c1013c8fd0356b7d5b517ceb6bea
Matt Hogeboom <matthogeboom@gmail.com>
2021-02-26 09:29:15 -0800

    10.6 - mobile hero updates

--- js/image-switch.js
+++ js/image-switch.js
@@ -4,7 +4,7 @@ image[1] = "images/blue-jeep-renegade-front.jpg";
 image[2] = "images/ford-range-rover-front.jpg";
 image[3] = "images/silver-subaru-forrester.jpg";
 image[4] = "images/silver-porsche-targa.jpg";
-var size = image.length
-var x = Math.floor(size*Math.random())
+var size = image.length;
+var x = Math.floor(size*Math.random());
 
 $('#random').attr('src',image[x]);
